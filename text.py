eb6151993182e3ee29137179e2723a46bfa3cd58
Mateusz <mzwigura@gmail.com>
2023-03-07 21:48:02 +0100

    Oddzielenie tekstow menu do text.py

---
+++ text.py
@@ -0,0 +1,38 @@
+
+menu_start_1 = "Stwórz nowy Budżet"
+menu_start_2 = "Wczystaj Budżet"
+menu_start_3 = "Lista budżetów"
+menu_start_4 = "Opcje"
+menu_start_5 = "Wyjdz"
+
+menu_options_1 = "Waluty"
+menu_options_2 = "Dodaj Walute"
+menu_options_3 = "Cofnij"
+
+menu_budzet_1 = "Dodaj nowy rok"
+menu_budzet_2 = "Wybierz rok"
+menu_budzet_3 = "Podsumowanie budżetu roczne"
+menu_budzet_4 = "Podsumowanie budżetu miesięczne"
+menu_budzet_5 = "Zmień ustawienia budżetu"
+menu_budzet_6 = "Cofnij"
+menu_budzet_7 = "Zakończ"
+
+
+menu_2_1 = "Wybierz Miesiąc"
+menu_2_2 = "Brak"
+menu_2_3 = "Wyświetl podsumowanie roku"
+menu_2_4 = "Brak"
+menu_2_5 = "Brak"
+menu_2_6 = "Cofnij"
+
+menu_month_1 = "Dodaj prognozowany przychód"
+menu_month_2 = "Dodaj prognozowany wydatek"
+menu_month_3 = "Dodaj rzeczywisty przychód"
+menu_month_4 = "Dodaj rzeczwisty wydatek"
+menu_month_5 = "Wyświetl szczegóły miesiąca"
+menu_month_6 = "Podsumowanie miesiąca"
+menu_month_7 = "Zamknij miesiac"
+menu_month_8 = "Cofnij"
+
+yes = "Tak"
+no = "Nie"
